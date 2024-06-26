5cc450fe3abca03586fc52cdeec279c0f0fc931c
Milad Joodi <winbeta.com@gmail.com>
2024-06-26 13:13:05 +0330

    up

---
+++ components/layout/posts/PostForm.tsx
@@ -0,0 +1,18 @@
+import { useForm } from "react-hook-form";
+import { PostSchema, PostSchemaType } from "../../../schemas/PostSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const PostForm = () => {
+
+    const { register, handleSubmit, reset, formState: {errors} } = useForm<PostSchemaType>({
+        resolver: zodResolver(PostSchema)
+    })
+
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default PostForm;  
