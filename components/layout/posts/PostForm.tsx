9999f6b509e72ef9f9ac79e9d7ed489ae1db94f5
Milad Joodi <winbeta.com@gmail.com>
2024-06-26 14:57:33 +0330

    Update PostForm.tsx

--- components/layout/posts/PostForm.tsx
+++ components/layout/posts/PostForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { PostSchema, PostSchemaType } from "../../../schemas/PostSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import FormField from "../../form/FormField";
@@ -12,8 +12,12 @@ const PostForm = () => {
         resolver: zodResolver(PostSchema)
     })
 
+    const onSubmit: SubmitHandler<PostSchemaType> = (data)=>{
+        alert(data);
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <FormField id="title" register={register} errors={errors} placeholder="Title" disabled={loading}  />
         </form>
     );
