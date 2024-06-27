cdbf8d043098d38e89e9c443af47f2d58ae908a1
Milad Joodi <winbeta.com@gmail.com>
2024-06-27 18:24:20 +0330

    up

--- components/layout/posts/DeletePost.tsx
+++ components/layout/posts/DeletePost.tsx
@@ -4,15 +4,22 @@ import { Post } from "@prisma/client";
 import { useState } from "react";
 import { ImSpinner } from "react-icons/im";
 import { MdDelete } from "react-icons/md";
+import { deletePost } from "../../../actions/postServerActions";
 
 const DeletePost = ({post}: {post: Post}) => {
 
     const [loading, setLoading] = useState(false);
 
+    const handleDelete = async (post: Post)=> {
+        setLoading(true)
+        await deletePost(post)
+        setLoading(false)
+    }
+
     return (
-        <button>
+        <button className="text-slate-500" onClick={()=> handleDelete(post)}>
             {!loading && <MdDelete size={20} />}
-            {loading && <ImSpinner size={20} />}
+            {loading && <ImSpinner size={20} className="animate-spin" />}
         </button>
     );
 }
