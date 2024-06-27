e9843b92f4e3662a41229c374c9bfe3d53c3a184
Milad Joodi <winbeta.com@gmail.com>
2024-06-27 12:37:45 +0330

    Update Header.tsx

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -10,7 +10,8 @@ interface HeadingProps {
 const Header = ({title, center, lg}: HeadingProps) => {
     return (
         <div className={center ? 'text-center' : 'text-start'}>
-            <h1 className={cn("font-bold text-2xl my-2", lg && "text-4xl")}>{title}</h1>
+            {lg && <h1 className={cn("font-bold text-4xl my-2")}>{title}</h1>}
+            
         </div>
     );
 }
