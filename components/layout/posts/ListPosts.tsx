79982f4022363158313b99c65b82a65f5a99330c
Milad Joodi <winbeta.com@gmail.com>
2024-06-28 21:26:47 +0330

    Update ListPosts.tsx

--- components/layout/posts/ListPosts.tsx
+++ components/layout/posts/ListPosts.tsx
@@ -8,7 +8,7 @@ const ListPosts = async () => {
     if(posts.success){
         return (
             <div className="flex flex-col max-w-[500px] m-auto">
-                {posts.success.map((post)=>{
+                {posts.success.map((post: any)=>{
                     return <div key={post.title} className="my-2 flex items-center gap3 justify-between w-full rounded p-2 border-b">
                         <span>{post.title}</span>
                         <div className="flex gap-2 items-center">
